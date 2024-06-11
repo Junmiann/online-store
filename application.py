6de0ffa14cb14905cc19eb3cf2958cc3fbbabf25
Junmiann <Mimmixu@gmail.com>
2024-06-11 16:36:07 +0200

    Added order details

--- application.py
+++ application.py
@@ -67,7 +67,26 @@ def user_profile():
     user = session.get('user')
     if user is None:
         return redirect(url_for("login"))
-    return render_template("user_profile.html", user=user)
+    else:
+        order_id = check_order_status()
+
+        cur = con.cursor()
+        cur.callproc("customer_orders", [user[0], order_id])
+        user_orders = cur.fetchall()
+        return render_template("user_profile.html", user=user, user_orders=user_orders)
+
+@app.route("/user_profile/order_details/<int:order_id>")
+def user_order_details(order_id):
+    user = session.get('user')
+    
+    cur = con.cursor()
+    cur.callproc("customer_orders", [user[0], order_id])
+    user_orders = cur.fetchall()
+    
+    cur.callproc("get_order_products", [order_id])
+    user_order_products = cur.fetchall()
+    
+    return render_template("order_details.html", user=user, order_id=order_id, user_orders=user_orders, user_order_products=user_order_products)
 
 @app.route("/logout")
 def logout():

172e474b051a00a93a8e3a99f683bea3d1d5cde0
Junmiann <Mimmixu@gmail.com>
2025-06-05 11:54:47 +0200

    Add check out confirmation
    Added confirmation pop up
    Changed flash message layout
    Added some comments

--- blueprints/cart.py
+++ blueprints/cart.py
@@ -11,11 +11,14 @@ def cart():
     user = session.get("user")
 
     if user:
+        # Check if user has an on-going order
         order_id = check_order_status()
 
         if order_id:
+            # Get all products (to display the added products into the cart)
             list_products = sp_all_products()
 
+            # Get order details and products from DB
             cur = con.cursor()
             cur.callproc("get_order_details", [order_id])
             user_order = cur.fetchall()
@@ -23,26 +26,27 @@ def cart():
             cur.callproc("get_order_products", [order_id])
             user_order_products = cur.fetchall()
 
+            # Render shopping cart page with all data
             return render_template("/cart.html", user=user, order_id=order_id, list_products=list_products, user_order=user_order, user_order_products=user_order_products)
         else:
             render_template("/cart.html", user=user)
-            
     else:
         flash("Log in to check your shopping cart!")
-        return render_template("/cart.html")
+        return redirect(url_for("login.login"))
 
 @cart_bp.route("/remove_item_from_cart/<int:product_id>", methods=["POST"])
 def remove_item_from_cart(product_id):
+    """
+    Remove a product from the user's shopping cart by product_id
+    """
     order_id = check_order_status()
 
     cur = con.cursor()
 
     cur.callproc("get_product_by_id", [product_id])
-    
     cur.fetchall()
 
     cur.callproc("delete_product", [order_id, product_id])
-
     con.commit()
     cur.close()
 
@@ -50,15 +54,26 @@ def remove_item_from_cart(product_id):
 
 @cart_bp.route("/cart/order_check_out/<int:order_id>", methods=["POST"])
 def order_check_out(order_id):
+    """
+    Process check out for the order with the given order_id
+    """
     user = session.get("user")
 
     order_id = check_order_status()
 
     cur = con.cursor()
+    cur.callproc("get_order_details", order_id)
+    order_details = cur.fetchall()
+    order_sum = order_details[0][2]
 
-    cur.callproc("order_check_out", [user[0], order_id])
+    if order_sum == 0:
+        flash("Please add an item into the cart before checking out!")
+        return redirect(url_for("cart.cart"))
+    
+    else: 
+        cur.callproc("order_check_out", [user[0], order_id])
+        con.commit()
+        cur.close()
 
-    con.commit()
-    cur.close()
-
-    return redirect(url_for("index"))
+        flash("Your purchase was successful!")
+        return redirect(url_for("cart.cart"))
--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,28 @@
-let quantity = document.querySelector("#quantity");
+// Quantity (product page)
+document.addEventListener("DOMContentLoaded", function() {
+    const quantity = document.querySelector("#quantity");
 
-const decrement = () => {
-    if(quantity.value <= 1) {
-        quantity.value = 1;
-    } else {
-        quantity.value = parseInt(quantity.value) - 1;
+    window.decrement = function() {
+        if(quantity.value <= 1) {
+            quantity.value = 1;
+        } else {
+            quantity.value = parseInt(quantity.value) - 1;
+        }
     }
-}
-const increment = () => {
-    if(quantity.value >= 10) {
-        quantity.value = 10;
-    } else {
-        quantity.value = parseInt(quantity.value) + 1;
+
+    window.increment = function() {
+        if(quantity.value >= 10) {
+            quantity.value = 10;
+        } else {
+            quantity.value = parseInt(quantity.value) + 1;
+        }
     }
-}
+});
+
+// Flash message
+setTimeout(function(){
+    var msg = document.getElementById("flash-msg");
+    if (msg) {
+        msg.style.display = "none";
+    }
+}, 4000);

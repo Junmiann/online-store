d5b61fec3b65dd11ffb39c98acf0cb7183a4ef4e
Junmiann <Mimmixu@gmail.com>
2025-07-18 13:49:15 +0200

    Refactor add_to_cart function

--- routes/cart.py
+++ routes/cart.py
@@ -9,20 +9,54 @@ app = Flask(__name__)
 cart_bp = Blueprint('cart', __name__, url_prefix='')
 
 def handle_cart_quantity_error(con, product_id):
-    # No changes if the chosen quantity exceeds the stock limit
     con.rollback()
     flash("Not enough in stock! The product was not added into your cart.")
     return redirect(url_for("product.product", product_id=product_id))
+
+def check_product_in_cart(con, order_id, product_id):
+    cur = con.cursor()
+    cur.callproc("get_product_by_id", [product_id])
+    product = cur.fetchone()
+    left_in_stock = product[6]
+    
+    cur.callproc("check_product_in_cart", [order_id, product_id])
+    product_in_cart = cur.fetchall()
+    return left_in_stock, product_in_cart
+
+def quantity_comparison(user, user_chosen_quantity, order_id, product_id):
+    """
+    Compares the user's selected quantity with available stock
+    If the quantity exceeds the available stock, an error is handled
+    """
+    left_in_stock, product_in_cart = check_product_in_cart(con, order_id, product_id)
+    cur = con.cursor()
+
+    if user_chosen_quantity > left_in_stock:
+        return False
+    elif product_in_cart:
+        product_quantity_in_cart = product_in_cart[0][3]
+        if product_quantity_in_cart + user_chosen_quantity > left_in_stock:
+            return False
+        else:
+            cur.callproc("update_cart_product_quantity", [user_chosen_quantity, order_id, product_id])
+    else:
+        cur.callproc("add_product", [order_id, user[0], product_id, user_chosen_quantity])
+
+    return True
+
+def update_total_price(order_id):
+    cur = con.cursor()
+    cur.callproc("update_order_total_price", [order_id])
+    con.commit()
+    cur.close()
 
 @cart_bp.route("/cart")
 def cart():
     user = session.get("user")
     if user:
-        # Check if user has an on-going order
         order_id = check_order_status()
 
         if order_id:
-            # Get all products (to display the added products into the cart)
             list_products = all_products()
 
             user_orders_details = UserOrder.get_user_order_details(con, order_id)
@@ -35,44 +69,21 @@ def cart():
         flash("Log in to check your shopping cart!")
         return redirect(url_for("login.login"))
 
-# TODO: The function is too long, shorten it for readability and maintainability
 @cart_bp.route("/add_to_cart/<int:product_id>", methods=["POST"])
 def add_to_cart(product_id):
     user = session.get("user")
-
     if not user:
         flash("Log in to add products to your shopping cart!")
         return redirect(url_for("login.login"))
     
     order_id = check_order_status()
-
-    cur = con.cursor()
-
     user_chosen_quantity = int(request.form["quantity"])
-    cur.callproc("get_product_by_id", [product_id])
-    product = cur.fetchone()
-    left_in_stock = product[6]
-    
-    cur.callproc("check_product_in_cart", [order_id, product_id])
-    existing_product_in_cart = cur.fetchall()
-    try:
-        if user_chosen_quantity > left_in_stock:
-            return handle_cart_quantity_error(con, product_id)
-        elif existing_product_in_cart:
-            product_quantity_in_cart = existing_product_in_cart[0][3] 
 
-            if product_quantity_in_cart + user_chosen_quantity > left_in_stock:
-                return handle_cart_quantity_error(con, product_id)
-            else:
-                cur.callproc("update_cart_product_quantity", [user_chosen_quantity, order_id, product_id])
-        else:
-            cur.callproc("add_product", [order_id, user[0], product_id, user_chosen_quantity])
-    except psycopg2.Error:
+    success = quantity_comparison(user, user_chosen_quantity, order_id, product_id)
+    if not success:
         return handle_cart_quantity_error(con, product_id)
 
-    cur.callproc("update_order_total_price", [order_id])
-    con.commit()
-    cur.close()
+    update_total_price(order_id)
 
     flash("The product has been added to your cart!")
     return redirect(url_for("product.product", product_id=product_id))
